a115635ec2e973acacc844d10aada71f71b5a020
Abdulmalik Adekunle <malikabdulazeez27@gmail.com>
2023-04-24 15:58:45 +0100

    first commit

---
+++ src/types/index.d.ts
@@ -0,0 +1,18 @@
+// declare module that don't have inbuilt typings;
+declare module "country-flags-svg";
+declare module "react-flags";
+
+// declare jpgs/pngs/svgs
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.svg";
+
+// interface view type
+export interface ViewType {
+    name: string;
+    pv: number;
+    uv: number;
+  }
+
+// graph data Type
+export type GraphDataType = [string[] | T , Dispatch<React.SetStateAction<never[]>>]
